8c99f3c5d881d5b4c1a6e7090b9866e5036acee7
San'Quan Prioleau <sq.prioleau@gmail.com>
2026-09-23 05:42:01 -0400

    🍱 Add logo assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Brightray",
+  title: "brightflare",
   description: "A childcare center front desk companion app that provides the answers parents need.",
 };
 
